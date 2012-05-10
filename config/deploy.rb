984ad35473d9c5c735c6054e950aab36adf15fbd
Jordan Byron <jordan.byron@gmail.com>
2012-05-10 16:14:02 -0400

    Add Capfile and configuration for webbynode

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+require 'bundler/capistrano'
+
+set :application, "anita"
+set :repository,  "git://github.com/mendicant/anita.git"
+
+set :scm, :git
+set :deploy_to, "/var/rapp/#{application}"
+
+set :user, "git"
+set :use_sudo, false
+
+set :deploy_via, :remote_cache
+
+set :branch, "master"
+server "mendicantuniversity.org", :app, :web, :db, :primary => true
+
+after 'deploy:update_code' do
+  run "ln -nfs #{shared_path}/environment.rb #{release_path}/config/environment.rb"
+  run "ln -nfs #{shared_path}/data #{release_path}/data"
+end
+
+namespace :deploy do
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after "deploy", 'deploy:cleanup'
