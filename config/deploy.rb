6482b51b0b1ea51a420dd307be93c0127950963f
Jordan Byron <jordan.byron@gmail.com>
2012-05-13 17:49:47 -0400

    Stop, reload, and start anita's god script after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,9 +14,18 @@ set :deploy_via, :remote_cache
 set :branch, "master"
 server "community.mendicantuniversity.org", :app, :web, :db, :primary => true
 
+before 'deploy:update_code' do
+  run "sudo god stop anita"
+end
+
 after 'deploy:update_code' do
   run "ln -nfs #{shared_path}/environment.rb #{release_path}/config/environment.rb"
   run "ln -nfs #{shared_path}/data #{release_path}/data"
+end
+
+after 'deploy' do
+  run "sudo god load #{release_path}/config/anita.god"
+  run "sudo god start anita"
 end
 
 namespace :deploy do
