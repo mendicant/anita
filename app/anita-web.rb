382e0a2cf46a304e3d7a23e7c955b8a631781f80
Shane Emmons <shane@emmons.io>
2012-05-14 11:27:58 -0400

    refactor anita-web.rb

--- app/anita-web.rb
+++ app/anita-web.rb
@@ -15,29 +15,61 @@ class AnitaWeb < Sinatra::Base
     "Oh hai, I'm Anita :]"
   end
 
-  get "/:channel/:from..:to.:format" do |channel, from, to, format|
+  get "/:channel/:from..:to.:format" do |channel, from, to, ext|
     messages = messages_for(channel, from, to)
+    format   = format_for(ext)
 
+    render_transcript(messages, format)
+  end
+
+  get "/:channel/:from..:to", provides: "html" do |channel, from, to|
+    messages = messages_for(channel, from, to)
+    render_transcript(messages)
+  end
+
+  private
+
+  def render_transcript(messages, format = :html)
     case format
-    when "html"
-      haml(:transcript, locals: {channel: channel, messages: messages})
-    when "json", "js"
+    when :html
+      haml(:transcript, locals: {messages: messages})
+    when :json
       messages.to_json
-    when "markdown", "md"
-      liquid(:transcript, locals: {channel: channel, messages: messages})
+    when :markdown
+      liquid(
+        :transcript,
+        locals: {channel: messages.channel, messages: messages}
+      )
     else
       raise Sinatra::NotFound
     end
   end
 
-  private
+  def format_for(ext)
+    case ext
+    when "html"
+      :html
+    when "json", "js"
+      :json
+    when "markdown", "md"
+      :markdown
+    else
+      :unknown
+    end
+  end
 
   def messages_for(channel, from, to)
     from = DateTime.parse(from).to_s
     to   = DateTime.parse(to).to_s
 
-    Anita::Storage::Statements::Read
+    messages = Anita::Storage::Statements::Read
       .execute("from" => from, "to" => to)
       .to_a
+
+    messages.define_singleton_method(:channel) do
+      channel
+    end
+
+    messages
   end
 end
