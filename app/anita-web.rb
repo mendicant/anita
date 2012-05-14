4a1e52341390ecfcaa3c04106f65bc2a34f492bb
Shane Emmons <shane@emmons.io>
2012-05-13 23:32:28 -0400

    handle respond_to ourselves

--- app/anita-web.rb
+++ app/anita-web.rb
@@ -1,5 +1,4 @@
 require "sinatra/base"
-require "sinatra/respond_to"
 require "date"
 require "json"
 require "haml"
@@ -8,10 +7,6 @@ require "liquid"
 require_relative "../lib/anita"
 
 class AnitaWeb < Sinatra::Base
-  register Sinatra::RespondTo
-
-  set :default_content, :html
-
   configure do
     mime_type :markdown, "text/plain"
   end
@@ -20,26 +15,29 @@ class AnitaWeb < Sinatra::Base
     "Oh hai, I'm Anita :]"
   end
 
-  get "/:channel/:from..:to" do |channel, from, to|
+  get "/:channel/:from..:to.:format" do |channel, from, to, format|
+    messages = messages_for(channel, from, to)
+
+    case format
+    when "html"
+      haml(:transcript, locals: {channel: channel, messages: messages})
+    when "json"
+      messages.to_json
+    when "markdown"
+      liquid(:transcript, locals: {channel: channel, messages: messages})
+    else
+      raise Sinatra::NotFound
+    end
+  end
+
+  private
+
+  def messages_for(channel, from, to)
     from = DateTime.parse(from).to_s
     to   = DateTime.parse(to).to_s
 
-    messages = Anita::Storage::Statements::Read
+    Anita::Storage::Statements::Read
       .execute("from" => from, "to" => to)
       .to_a
-
-    respond_to do |f|
-      f.html do
-        haml(:transcript, locals: {channel: channel, messages: messages})
-      end
-
-      f.json do
-        messages.to_json
-      end
-
-      f.markdown do
-        liquid(:transcript, locals: {channel: channel, messages: messages})
-      end
-    end
   end
 end
