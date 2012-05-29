59f54fc923daaa08ec80cea54d1f248a5fe07f45
Shane Emmons <semmons99@gmail.com>
2012-05-29 13:28:55 -0400

    add transcripts specs

--- lib/anita/transcripts.rb
+++ lib/anita/transcripts.rb
@@ -4,11 +4,11 @@ module Anita
     include Enumerable
 
     def self.record(channel, nick, text)
-      Messages.create(channel: channel, nick: nick, text: text)
+      Anita::Messages.create(channel: channel, nick: nick, text: text)
     end
 
     def self.load(channel, from, to)
-      messages = Messages.all(channel: channel, timestamp: (from..to))
+      messages = Anita::Messages.all(channel: channel, timestamp: (from..to))
       Transcripts.new(messages)
     end
 
@@ -17,12 +17,16 @@ module Anita
     attr_reader :channel
 
     def initialize(messages)
-      @messages = messages
-      @channel  = messages.first.channel
+      self.messages = messages
+      self.channel  = messages.first.channel
     end
 
     def to_json
       @messages.map(&:to_h).to_json
     end
+
+    private
+
+    attr_writer :channel, :messages
   end
 end
